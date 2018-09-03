9a881022f8915904e0a4c3746ffef33564103d09
aa3212689 <1064404481@qq.com>
2018-09-04 07:08:32 +0800

    赛事控制器
    包括比赛和用户

---
+++ sai/controller/AdminSaiController.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\sai\controller;
+use cmf\controller\AdminBaseController;
+
+class AdminSaiController extends AdminBaseController{
+
+    public function index(){
+        return $this->fetch();
+    }
+
+    public function add(){
+        return $this->fetch();
+    }
+
+}
